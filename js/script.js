ebcfd000cd26bad32610354efa35d7b62bda9f4e
abdelr7manwael <abdelr7manwael07@gmail.com>
2023-11-21 02:16:30 +0200

    part 1

--- js/script.js
+++ js/script.js
@@ -27,3 +27,25 @@ lightboxBtn.forEach(l => {
 lightboxClose.addEventListener('click', ()=>{
     lightboxBox.classList.add('d-none');
 })
+
+
+// menu section navs & tabs 
+
+var tabs = document.querySelectorAll(".menu-section .nav-item .nav-link");
+var pages = document.querySelectorAll(".menu-section .pages .page")
+
+tabs.forEach((t, index) =>{
+    t.addEventListener('click', ()=>{
+        tabs.forEach(subt => {
+            subt.classList.remove('active')
+        });
+
+        pages.forEach(p => {
+            p.classList.add('d-none')
+        });
+        t.classList.add('active');
+        pages[index].classList.remove('d-none')
+
+
+    })    
+})
